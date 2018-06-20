8775eb6b3a320683a34df07aa4658e8a6dca8508
mcauliffek4298 <kathryn@peerlift.org>
2018-06-20 16:48:14 +0000

    automatically backed up by learn

---
+++ array.rb
@@ -0,0 +1,27 @@
+# student_one="Caroline"
+# student_two= "Millie"
+# student_three="Sam"
+# student_four="Sonali"
+
+#puts "In this class, we have a student name #{student_four}"
+
+student_list=["Caroline","Millie", "Sonali", "Sam",]
+#puts student_list
+
+friends_list=["Phoebe", "Rachel", "Monica","Joey", "Ross","Chandler"]
+#puts friends_list
+
+disney_list=["Moanna","Elsa","Cinderella", "Mickey Mouse", "Mulan"]
+
+disney_list.push("Pooh Bear","Tiger")
+
+puts disney_list.length
+
+age_list= [16, 16, 14, 15]
+#puts age_list
+
+#book_list=["Harry Potter", "To Kill a Mockingbird", "A Tree Grows in Brooklyn", "Nineteen Minutes"]
+#puts book_list[0]
+
+#book_list.push("Wonder")
+#puts book_list
--- reading_arrays.rb
+++ reading_arrays.rb
@@ -4,29 +4,44 @@ STUDENT_NAMES = [
   "Cardi B",
   "Lady Gaga",
   "Nicki Minaj",
-  "Rihanna"
-]
+  "Rihanna"]
 
 def first_student_by_index
+ # puts STUDENT_NAMES[0]
+  
   # Write a solution that returns the first student in the array student_names
 end
+first_student_by_index
 
 def fourth_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  #puts STUDENT_NAMES[4]
 end
+fourth_student_by_index
 
 def last_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  # Write a solution that returns the first student in the array STUDENT_NAMES
+  #puts STUDENT_NAMES[5]
 end
+last_student_by_index
 
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
+  
+  #puts STUDENT_NAMES.first
 end
+first_student_by_method
 
 def last_student_by_method
   # Write a solution that returns the first student using the built in .last method
+  #puts STUDENT_NAMES.last
 end
+last_student_by_method
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
+  puts STUDENT_NAMES[0]
+  puts STUDENT_NAMES[1]
+  puts STUDENT_NAMES[2]
+  
 end
+first_second_and_third_students
